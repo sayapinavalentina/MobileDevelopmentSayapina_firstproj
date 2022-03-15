6e8f9e194739f2a02e6c951339fc3ea91cfbf598
sayapinavalentina <sayapinavalentina7@gmail.com>
2022-03-15 13:55:58 +0300

    Add a launchscreen, mainpage with button that return text into field in alert

--- FirstLessonSayapina/ViewController.swift
+++ FirstLessonSayapina/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textfieldView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textfieldView.layer.borderWidth = 1
+        textfieldView.layer.borderColor = UIColor.systemPurple.cgColor
+        textField.attributedPlaceholder = NSAttributedString(string: "Add some text..", attributes: [NSAttributedString.Key.foregroundColor : UIColor.purple])
+        textfieldView.layer.cornerRadius = 4
     }
 
-
+    @IBAction func touchMeButton(_ sender: Any)
+    {
+        if !textField.text!.isEmpty
+        {
+            showAlert(name: "Success", desc: textField.text!)
+        }
+        else
+        {
+            showAlert(name: "Error", desc: "No data in field")
+        }
+    }
+    
+    func showAlert(name: String, desc: String)
+    {
+        let alert = UIAlertController(title: name, message: desc, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
